a736e39206c406c13ed4228c6d1a4cc2898533c8
sanrom <sanrom9776@gmail.com>
2025-03-31 16:20:18 +0100

    updated alliance numbers to 8 full, 8 regional

--- airline-data/src/main/scala/com/patson/model/Alliance.scala
+++ airline-data/src/main/scala/com/patson/model/Alliance.scala
@@ -2,42 +2,92 @@ package com.patson.model
 
 import scala.collection.mutable.ListBuffer
 import com.patson.data.{AllianceSource, CountrySource, CycleSource}
-import com.patson.model.AllianceEvent.{BOOT_ALLIANCE, LEAVE_ALLIANCE, PROMOTE_LEADER, REJECT_ALLIANCE}
+import com.patson.model.AllianceEvent.{
+  BOOT_ALLIANCE,
+  LEAVE_ALLIANCE,
+  PROMOTE_LEADER,
+  REJECT_ALLIANCE
+}
 import com.patson.util.{AirlineCache, ChampionUtil}
 import play.api.libs.json.Json
 
 import scala.math.BigDecimal.RoundingMode
 
-case class Alliance(name: String, creationCycle: Int, members: List[AllianceMember], var id: Int = 0) {
+case class Alliance(
+    name: String,
+    creationCycle: Int,
+    members: List[AllianceMember],
+    var id: Int = 0
+) {
   val status = {
-    if (members.filter(_.role != AllianceRole.APPLICANT).length < Alliance.ESTABLISH_MIN_MEMBER_COUNT) {
+    if (
+      members
+        .filter(_.role != AllianceRole.APPLICANT)
+        .length < Alliance.ESTABLISH_MIN_MEMBER_COUNT
+    ) {
       AllianceStatus.FORMING
     } else {
       AllianceStatus.ESTABLISHED
     }
   }
 
-  def removeMember(member : AllianceMember, removeSelf : Boolean) : Unit = {
+  def removeMember(member: AllianceMember, removeSelf: Boolean): Unit = {
     import AllianceRole._
     val targetAirlineId = member.airline.id
     val currentCycle = CycleSource.loadCycle()
-    if (members.find(_.airline.id == targetAirlineId).isDefined) { //just to play safe
+    if (members.find(_.airline.id == targetAirlineId).isDefined) { // just to play safe
       AllianceSource.deleteAllianceMember(targetAirlineId)
       if (removeSelf) {
-        AllianceSource.saveAllianceHistory(AllianceHistory(allianceName = name, airline = member.airline, event = LEAVE_ALLIANCE, cycle = currentCycle))
+        AllianceSource.saveAllianceHistory(
+          AllianceHistory(
+            allianceName = name,
+            airline = member.airline,
+            event = LEAVE_ALLIANCE,
+            cycle = currentCycle
+          )
+        )
         if (member.role == LEADER) {
-          members.filter(_.role == CO_LEADER).sortBy(_.joinedCycle).headOption match {
+          members
+            .filter(_.role == CO_LEADER)
+            .sortBy(_.joinedCycle)
+            .headOption match {
             case Some(coLeader) =>
-              AllianceSource.saveAllianceMember(coLeader.copy(role = AllianceRole.LEADER))
-              AllianceSource.saveAllianceHistory(AllianceHistory(allianceName = name, airline = coLeader.airline, event = PROMOTE_LEADER, cycle = currentCycle))
-            case None => AllianceSource.deleteAlliance(id) //no co-leader, remove the alliance
+              AllianceSource.saveAllianceMember(
+                coLeader.copy(role = AllianceRole.LEADER)
+              )
+              AllianceSource.saveAllianceHistory(
+                AllianceHistory(
+                  allianceName = name,
+                  airline = coLeader.airline,
+                  event = PROMOTE_LEADER,
+                  cycle = currentCycle
+                )
+              )
+            case None =>
+              AllianceSource.deleteAlliance(
+                id
+              ) // no co-leader, remove the alliance
           }
         }
       } else {
         if (member.role == APPLICANT) {
-          AllianceSource.saveAllianceHistory(AllianceHistory(allianceName = name, airline = member.airline, event = REJECT_ALLIANCE, cycle = currentCycle))
+          AllianceSource.saveAllianceHistory(
+            AllianceHistory(
+              allianceName = name,
+              airline = member.airline,
+              event = REJECT_ALLIANCE,
+              cycle = currentCycle
+            )
+          )
         } else {
-          AllianceSource.saveAllianceHistory(AllianceHistory(allianceName = name, airline = member.airline, event = BOOT_ALLIANCE, cycle = currentCycle))
+          AllianceSource.saveAllianceHistory(
+            AllianceHistory(
+              allianceName = name,
+              airline = member.airline,
+              event = BOOT_ALLIANCE,
+              cycle = currentCycle
+            )
+          )
         }
       }
     }
@@ -50,33 +100,48 @@ object AllianceStatus extends Enumeration {
   val ESTABLISHED, FORMING = Value
 }
 
-case class AllianceMember(allianceId: Int, airline: Airline, role: AllianceRole.Value, joinedCycle: Int)
+case class AllianceMember(
+    allianceId: Int,
+    airline: Airline,
+    role: AllianceRole.Value,
+    joinedCycle: Int
+)
 
 object AllianceRole extends Enumeration {
   type AllianceRole = Value
   val LEADER, CO_LEADER, MEMBER, APPLICANT = Value
 
-  val isAdmin : AllianceRole => Boolean = (role : AllianceRole) => role match {
-    case LEADER | CO_LEADER => true
-    case _ => false
-  }
+  val isAdmin: AllianceRole => Boolean = (role: AllianceRole) =>
+    role match {
+      case LEADER | CO_LEADER => true
+      case _                  => false
+    }
 
-  val isAccepted : AllianceRole => Boolean = (role : AllianceRole) => role match {
-    case APPLICANT => false
-    case _ => true
-  }
+  val isAccepted: AllianceRole => Boolean = (role: AllianceRole) =>
+    role match {
+      case APPLICANT => false
+      case _         => true
+    }
 }
 
-case class AllianceHistory(allianceName: String, airline: Airline, event: AllianceEvent.Value, cycle: Int, var id: Int = 0)
+case class AllianceHistory(
+    allianceName: String,
+    airline: Airline,
+    event: AllianceEvent.Value,
+    cycle: Int,
+    var id: Int = 0
+)
 
 object AllianceEvent extends Enumeration {
   type AllianceEvent = Value
-  val FOUND_ALLIANCE, APPLY_ALLIANCE, JOIN_ALLIANCE, REJECT_ALLIANCE, LEAVE_ALLIANCE, BOOT_ALLIANCE, PROMOTE_LEADER, PROMOTE_CO_LEADER, DEMOTE = Value
+  val FOUND_ALLIANCE, APPLY_ALLIANCE, JOIN_ALLIANCE, REJECT_ALLIANCE,
+      LEAVE_ALLIANCE, BOOT_ALLIANCE, PROMOTE_LEADER, PROMOTE_CO_LEADER, DEMOTE =
+    Value
 }
 
 object Alliance {
-  val MAX_MEMBER_NON_REGIONAL_COUNT = 5
-  val MAX_MEMBER_REGIONALS_COUNT = 2
+  val MAX_MEMBER_NON_REGIONAL_COUNT = 8
+  val MAX_MEMBER_REGIONALS_COUNT = 8
   val ESTABLISH_MIN_MEMBER_COUNT = 3
 
   val getReputationBonus: (Int => Double) = { (ranking: Int) =>
@@ -95,39 +160,50 @@ object Alliance {
     }
   }
 
-  /**
-    *
-    * @param alliances
-    * @return Map[Alliance, (ranking, champion points)] . Take note that ranking starts with 1 as the top alliance
+  /** @param alliances
+    * @return
+    *   Map[Alliance, (ranking, champion points)] . Take note that ranking
+    *   starts with 1 as the top alliance
     */
-  def getRankings(alliances: List[Alliance]): Map[Alliance, (Int, BigDecimal)] = {
-    //val countryChampions = ChampionUtil.getAllCountryChampionInfo()
-    val airportChampionsByAirlineId = ChampionUtil.loadAirportChampionInfo().groupBy(_.loyalist.airline.id)
-    val alliancesWithChampionPoints: List[(Alliance, BigDecimal)] = alliances.filter(_.status == AllianceStatus.ESTABLISHED).map {
-      alliance =>
+  def getRankings(
+      alliances: List[Alliance]
+  ): Map[Alliance, (Int, BigDecimal)] = {
+    // val countryChampions = ChampionUtil.getAllCountryChampionInfo()
+    val airportChampionsByAirlineId =
+      ChampionUtil.loadAirportChampionInfo().groupBy(_.loyalist.airline.id)
+    val alliancesWithChampionPoints: List[(Alliance, BigDecimal)] =
+      alliances.filter(_.status == AllianceStatus.ESTABLISHED).map { alliance =>
         var allianceChampionPoints: BigDecimal = 0.0
         alliance.members.foreach { allianceMember =>
           val memberChampionPoints: BigDecimal =
-            if (allianceMember.role == AllianceRole.APPLICANT) { //do not add champion points from applicant
+            if (allianceMember.role == AllianceRole.APPLICANT) { // do not add champion points from applicant
               0
             } else {
-              BigDecimal(airportChampionsByAirlineId.get(allianceMember.airline.id).map(_.map(_.reputationBoost).sum).getOrElse(0.0))
+              BigDecimal(
+                airportChampionsByAirlineId
+                  .get(allianceMember.airline.id)
+                  .map(_.map(_.reputationBoost).sum)
+                  .getOrElse(0.0)
+              )
             }
-          //println(s"${allianceMember.airline.name} => " + memberChampionPoints)
+          // println(s"${allianceMember.airline.name} => " + memberChampionPoints)
           allianceChampionPoints = allianceChampionPoints + memberChampionPoints
         }
 
         (alliance, allianceChampionPoints.setScale(2, RoundingMode.HALF_UP))
-    }
+      }
 
-    val alliancesWithRanking = alliancesWithChampionPoints.sortBy(_._2)(Ordering.BigDecimal.reverse).zipWithIndex.map {
-      case ((alliance, allianceChampionPoints), sortedIndex) => (alliance, (sortedIndex + 1, allianceChampionPoints))
-    }
+    val alliancesWithRanking = alliancesWithChampionPoints
+      .sortBy(_._2)(Ordering.BigDecimal.reverse)
+      .zipWithIndex
+      .map { case ((alliance, allianceChampionPoints), sortedIndex) =>
+        (alliance, (sortedIndex + 1, allianceChampionPoints))
+      }
 
     alliancesWithRanking.toMap
   }
 
-  def fromId(id : Int) = {
+  def fromId(id: Int) = {
     Alliance("<unknown>", 0, List.empty, id)
   }
 }
