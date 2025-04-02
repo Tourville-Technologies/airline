a72e62fbab18785e6378dc378ba2c031d8335853
sanrom <sanrom9776@gmail.com>
2025-04-02 10:30:50 +0000

    v3 changes

--- .docker/data/start.sh
+++ .docker/data/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "===== START OF BACKEND ====="
 cd /home/airline/airline/airline-data
-SBT_OPTS="-Xmx4G -Xms512M -XX:MaxMetaspaceSize=512M" sbt "runMain com.patson.MainSimulation"
+SBT_OPTS="-Xmx8G -Xms512M -XX:+UseG1GC -XX:MaxMetaspaceSize=512M" sbt "runMain com.patson.MainSimulation"
 echo "===== BACKEND SHUTDOWN WITH CODE $? ====="
